c17a06571a76dfb57e70f94e0a8df9aafb8fadd6
taboka <nydtab001@myuct.ac.za>
2025-10-03 22:16:03 +0200

    livestream update

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -91,7 +91,7 @@ function Home(){
   <h2 className="md:text-5xl max-sm:text-2xl max-sm:mb-8 pt-8 text-3xl font-semibold mb-12">Sabbath Livestream</h2>
   <div className="relative w-full max-w-4xl mx-auto aspect-video ">
     <iframe
-      src="https://www.youtube.com/embed/0pB5U0vFARU?si=KSJCt_H3ybf-FXHW"
+      src="https://www.youtube.com/embed/09WjXQJZMKg?si=a187uUgJ5DkLjnhI"
       title="Sabbath Livestream"
       className="absolute inset-0 w-full h-full"
       frameBorder="0"
